ab1672de7225b83d27ac249ac292f4cdc7b021a4
pumanitro <pumanitro@gmail.com>
2017-11-19 12:59:10 +0100

    Added jest, and tried to create usable Models.

---
+++ Models.ts
@@ -0,0 +1,74 @@
+export enum BoardTile {
+    Empty = 0,
+    Regular = 1,
+    Fortified = 2,
+    Indestructible = 3
+}
+
+export class BotLocation {
+    x: number;
+    y: number;
+
+    constructor(locationString: string){
+        this.x = Number(locationString[0]);
+        this.x = Number(locationString[0]);
+    }
+}
+
+export interface IBattleFieldInfo{
+    RoundNumber: number; //"RoundNumber": 3,
+    Board: Array<Array<BoardTile>> //"Board": [[2,2,3],[0,0,1],[0,0,0]]
+    BotLocation
+}
+
+/*
+
+{
+
+    "BotId": "a88454b0‐80ba‐4c10‐b162‐f1ca766f1e3f",
+    "Board": [
+    [
+        2,
+        2,
+        3
+    ],
+    [
+        0,
+        0,
+        1
+    ],
+    [
+        0,
+        0,
+        0
+    ]
+],
+    "BotLocation": "0, 0",
+    "IsMissileAvailable": true,
+    "OpponentLocations": [
+    "1, 1"
+],
+    "Bombs": [
+    {
+        "RoundsUntilExplodes": 3,
+        "Location": "0, 1",
+        "ExplosionRadius": 2
+    }
+],
+    "Missiles": [
+    {
+        "MoveDirection": 3,
+        "Location": "1, 0",
+        "ExplosionRadius": 2
+    }
+],
+    "GameConfig": {
+    "MapWidth": 20,
+        "MapHeight": 20,
+        "BombBlastRadius": 2,
+        "MissileBlastRadius": 2,
+        "RoundsBetweenMissiles": 5,
+        "RoundsBeforeIncreasingBlastRadius": 70,
+        "IsFastMissileModeEnabled": true
+}
+}*/
--- server.ts
+++ server.ts
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const AlphaBetaConstructor = require('alphabeta');
+
+let bodyParser = require('body-parser');
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+
 app.get('/Info', (req, res) => res.send({
     "Name": "IntelPJ",
     "AvatarUrl": "http://localhost/img/avatar.jpg",
@@ -8,10 +18,23 @@ app.get('/Info', (req, res) => res.send({
     "GameType": "TankBlaster"
 }));
 
-app.post('/PerformNextMove', (req, res) => res.send({
+app.post('/PerformNextMove', (req, res) => {
+
+    const config = {
+        scoreFunction 		: scoreFunction,
+        generateMoves		: generateMovesFunction,
+        checkWinConditions 	: checkWinConditionsFunction,
+        uniqueKey		: uniqueKeyFunction,
+        state 			: yourInitialStateObject,
+        depth 			: 1
+    };
+
+    const alphabeta = AlphaBetaConstructor( config );
+
+    res.send({
     "Direction": 1,
     "Action": 0,
     "FireDirection": 0
-}));
+})});
 
 app.listen(9970, () => console.log('IntelPJ is going to kick your as!'));
