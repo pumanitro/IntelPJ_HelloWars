db9b1254813c8dcb93754a5c473c64abe1292169
pumanitro <pumanitro@gmail.com>
2017-11-22 22:01:53 +0100

    Wrote tests for isSomeBombOnLocation. And created mocked states which can be inherited for tests.

--- Models/Missile.test.ts
+++ Models/Missile.test.ts
@@ -1,7 +1,25 @@
 import {isSomeBombOnLocation} from "./Missile";
+import {Location} from "./GameModels";
+import Bomb from "./Bomb";
+import {State} from "./AlphaBetaModels";
+import {MockedBattleFieldInfo} from "./MockState";
+
+let mockGameState  = new State(MockedBattleFieldInfo);
 
 describe('isSomeBombOnLocation', () => {
+
+    mockGameState.Bombs.push(new Bomb(
+        1,
+        new Location('9, 9'),
+        2
+    ));
+
     it('should return true if there is a bomb in a given state on given location', () => {
-        isSomeBombOnLocation()
+        expect(isSomeBombOnLocation(new Location('9, 9'), mockGameState)).toBe(true);
     });
+
+    it("should return false if there isn't a bomb in a given state on given location", () => {
+        expect(isSomeBombOnLocation(new Location('5, 5'), mockGameState)).toBe(false);
+    });
+
 });
---
+++ Models/MockState.ts
@@ -0,0 +1,55 @@
+import {BattleFieldInfo} from "./GameModels";
+import {State} from "./AlphaBetaModels";
+
+let sampleGameData = {
+    "RoundNumber": 3,
+    "BotId": "a88454b0‐80ba‐4c10‐b162‐f1ca766f1e3f",
+    "Board": [
+        [
+            2,
+            2,
+            3
+        ],
+        [
+            0,
+            0,
+            1
+        ],
+        [
+            0,
+            0,
+            0
+        ]
+    ],
+    "BotLocation": "0, 0",
+    "IsMissileAvailable": true,
+    "OpponentLocations": [
+        "1, 1"
+    ],
+    "Bombs": [
+        {
+            "RoundsUntilExplodes": 3,
+            "Location": "0, 1",
+            "ExplosionRadius": 2
+        }
+    ],
+    "Missiles": [
+        {
+            "MoveDirection": 3,
+            "Location": "1, 0",
+            "ExplosionRadius": 2
+        }
+    ],
+    "GameConfig": {
+        "MapWidth": 20,
+        "MapHeight": 20,
+        "BombBlastRadius": 2,
+        "MissileBlastRadius": 2,
+        "RoundsBetweenMissiles": 5,
+        "RoundsBeforeIncreasingBlastRadius": 70,
+        "IsFastMissileModeEnabled": true
+    }
+};
+
+export let MockedBattleFieldInfo = new BattleFieldInfo(sampleGameData);
+export let MockedGameState = new State(MockedBattleFieldInfo);
