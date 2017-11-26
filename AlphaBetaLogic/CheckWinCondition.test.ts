d6d7c7ed7141592ef46f9c2a23510c7d1062da7b
pumanitro <pumanitro@gmail.com>
2017-11-26 13:25:51 +0100

    Added checkWinConditionsFunction content. Need to refactor explosion output.

--- AlphaBetaLogic/CheckWinCondition.test.ts
+++ AlphaBetaLogic/CheckWinCondition.test.ts
@@ -1,11 +1,44 @@
 import {calcRecursivelyExplosionsArray} from './CheckWinCondition';
+import {MockedGameState} from "../Models/MockState";
+import {Bomb} from "../Models/Explodable/Bomb/Bomb";
+import {Location} from "../Models/Location";
+import Missile from "../Models/Explodable/Missile/Missile";
+import {MoveDirection} from "../Models/GameModels";
 
 describe('calcRecursivelyExplosionsArray', () => {
+
+    let mockState = MockedGameState;
+
+    it('should return array of explosion without any possible chain reaction', () => {
+
+        mockState.Bombs = [
+            new Bomb(0, new Location("2, 0"), 1)
+        ];
+
+        mockState.Missiles = [
+            new Missile(MoveDirection.Left, new Location("0, 2"), 1)
+        ];
+
+        let copyOfBombs = new Set();
+        mockState.Bombs.forEach(bomb => {
+            copyOfBombs.add(bomb.Location.generateKey(), new Bomb(bomb.RoundsUntilExplodes, new Location(bomb.Location.generateKey()), bomb.ExplosionRadius));
+        });
+
+        let copyOfMissiles = new Set();
+        mockState.Missiles.forEach(missile => {
+            copyOfMissiles.add(missile.Location.generateKey(), new Missile(missile.MoveDirection, new Location(missile.Location.generateKey()), missile.ExplosionRadius));
+        });
+
+        let explosionArray = [];
+        calcRecursivelyExplosionsArray(mockState, copyOfBombs, copyOfMissiles,  explosionArray);
+
+        expect(explosionArray).toEqual([
+
+        ]);
+    });
+
     it('should return array of explosions after chain reaction.', () => {
 
     });
 
-    it('should return array of explosions without the same Locations', () => {
-
-    });
 });
--- AlphaBetaLogic/CheckWinCondition.ts
+++ AlphaBetaLogic/CheckWinCondition.ts
@@ -8,12 +8,14 @@ export function calcRecursivelyExplosionsArray(state: State, actualBombs: Set<Bo
     for(let bomb of actualBombs) {
         if(bomb.shouldExplode()) {
             explosionsArray.concat(bomb.explode(state));
+            actualBombs.delete(bomb.Location.generateKey());
         }
     }
 
     for(let missile of actualMissiles) {
         if(missile.shouldExplode(state)) {
             explosionsArray.concat(missile.explode(state));
+            actualBombs.delete(missile.Location.generateKey());
         }
     }
 
@@ -22,6 +24,18 @@ export function calcRecursivelyExplosionsArray(state: State, actualBombs: Set<Bo
 
 export default function checkWinConditionsFunction(actualState: State ) {
 
+    let coppyOfBombs = new Set();
+    actualState.Bombs.forEach(bomb => {
+        coppyOfBombs.add(bomb.Location.generateKey(), new Bomb(bomb.RoundsUntilExplodes, new Location(bomb.Location.generateKey()), bomb.ExplosionRadius));
+    });
 
+    let coppyOfMissiles = new Set();
+    actualState.Missiles.forEach(missile => {
+        coppyOfBombs.add(missile.Location.generateKey(), new Missile(missile.MoveDirection, new Location(missile.Location.generateKey()), missile.ExplosionRadius));
+    });
+
+    let explosionArray = [];
+
+    calcRecursivelyExplosionsArray(actualState, coppyOfBombs, coppyOfMissiles,  explosionArray);
 
 }
